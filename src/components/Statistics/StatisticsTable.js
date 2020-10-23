8fd6da8ec97ea9c7db690d6af3e2136cfdd4b7e3
Sunho <Sunho@DESKTOP-7OV12KD>
2020-10-22 21:07:38 -0400

    .

--- src/components/Statistics/StatisticsTable.js
+++ src/components/Statistics/StatisticsTable.js
@@ -2,17 +2,11 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Button } from "@material-ui/core";
 import ACTIONS from "../../redux/actions";
-import SelectScalingNormalization from "../SelectScalingNormalization";
 
 const StatisticsTable = props => {
   const dispatch = useDispatch();
   const { statsData } = useSelector(state => state);
-  const [columns, setColumns] = useState([]);
-
-  useEffect(() => {
-    var arrData = 
-  }, [])
-
+  
   return (
     <div>
       <Button
@@ -22,7 +16,6 @@ const StatisticsTable = props => {
       >
         Test Data
       </Button>
-      <SelectScalingNormalization></SelectScalingNormalization>
       <br />
       {JSON.stringify(statsData)}
     </div>
